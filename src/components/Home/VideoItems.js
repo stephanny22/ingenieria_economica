ea350e3799ae43b3a5e8f78b18aacde4b8fe103f
Julian Rincon <julian1918@hotmail.es>
2022-12-11 18:53:53 -0500

    Arreglo de texto

--- src/components/Home/VideoItems.js
+++ src/components/Home/VideoItems.js
@@ -1,7 +1,7 @@
 export const VideoItems = ({ link, name }) => {
   return (
     <div className="col-md-4 col-lg-3">
-      <h5>{name}</h5>
+      <h5 className="text-truncate">{name}</h5>
       <div className="ratio ratio-16x9">
         <iframe src={link} title="YouTube video" allowFullScreen></iframe>
       </div>
