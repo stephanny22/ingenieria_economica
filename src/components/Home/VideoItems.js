b9f5982389e320560c405762aceaafdef2632d63
Julian Rincon <julian1918@hotmail.es>
2022-12-11 18:51:56 -0500

    Actualizacion de videos

--- src/components/Home/VideoItems.js
+++ src/components/Home/VideoItems.js
@@ -2,8 +2,8 @@ export const VideoItems = ({ link, name }) => {
   return (
     <div className="col-md-4 col-lg-3">
       <h5>{name}</h5>
-      <div class="ratio ratio-16x9">
-        <iframe src={link} title="YouTube video" allowfullscreen></iframe>
+      <div className="ratio ratio-16x9">
+        <iframe src={link} title="YouTube video" allowFullScreen></iframe>
       </div>
     </div>
   );
